14e9372d1ec3491fc49f44732ea817d12df22810
Luis Valdivia <luis.edvaldivia@gmail.com>
2022-01-04 15:21:41 -0800

    added sidebarlink

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,18 +1,39 @@
 import Image from "next/image";
+import { HomeIcon } from "@heroicons/react/solid";
+import {
+  HashtagIcon,
+  BellIcon,
+  InboxIcon,
+  BookmarkIcon,
+  ClipboardListIcon,
+  UserIcon,
+  DotsCircleHorizontalIcon,
+  DotsHorizontalIcon,
+} from "@heroicons/react/outline";
 import SidebarLink from "./SidebarLink";
 
 function Sidebar() {
   return (
-    <div className="hidden sm:flex flex-col items-center xl:items-start xl:w-[340px]
-    p-2 fixed h-full">
-      
-      <div className="flex items-center justify-center w-14 h-14 
-      hoverAnimation p-0 xl:ml-24" >
+    <div
+      className="hidden sm:flex flex-col items-center xl:items-start xl:w-[340px]
+    p-2 fixed h-full"
+    >
+      <div
+        className="flex items-center justify-center w-14 h-14 
+      hoverAnimation p-0 xl:ml-24"
+      >
         <Image src="https://rb.gy/ogau5a" width={30} height={30} />
       </div>
 
       <div className="space-y-2.5 mt-4 mb-2.5 xl:ml-24">
-        <SidebarLink />
+        <SidebarLink text="Home" Icon={HomeIcon} active />
+        <SidebarLink text="Explore" Icon={HashtagIcon} active />
+        <SidebarLink text="Notifications" Icon={BellIcon} active />
+        <SidebarLink text="Messages" Icon={InboxIcon} active />
+        <SidebarLink text="Bookmarks" Icon={BookmarkIcon} active />
+        <SidebarLink text="Lists" Icon={ClipboardListIcon} active />
+        <SidebarLink text="Profile" Icon={UserIcon} active />
+        <SidebarLink text="More" Icon={DotsCircleHorizontalIcon} active />
       </div>
     </div>
   );
--- components/SidebarLink.js
+++ components/SidebarLink.js
@@ -1,5 +1,10 @@
 function SidebarLink({ Icon, text, active }) {
-  return <div>Sidebarlink</div>;
+  return (
+    <div className={`text-[#d9d9d9] flex items-center justify-center xl:justify-start`}>
+      <Icon className="h-7" />
+      <span>{text}</span>
+    </div>
+  );
 }
 
 export default SidebarLink;
