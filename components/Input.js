42149cb3ae57d801c6ebf306b7ae6df447e2cc83
Luis Valdivia <luis.edvaldivia@gmail.com>
2022-01-04 17:32:29 -0800

    added text area

---
+++ components/Input.js
@@ -0,0 +1,21 @@
+function Input() {
+  return (
+    <div className={`border-b border-gray-700 p-3 flex space-x-3 overflow-y-scroll`}>
+      <img
+        src="https://media-exp1.licdn.com/dms/image/D5635AQEyNbzjaS9vpw/profile-framedphoto-shrink_100_100/0/1641261133658?e=1641427200&v=beta&t=Q2BizxXqz07bAzGiayk2wsdeRiOaXeHZRZ_EvCFHVQ8"
+        alt=""
+        className="h-11 w-11 rounded-full cursor-pointer"
+      />
+
+      <div className="w-full divide-y divide-gray-700">
+        <div>
+          <textarea name="" id="" cols="30" rows="10"></textarea>
+        </div>
+
+        <div>osaujdfh</div>
+      </div>
+    </div>
+  );
+}
+
+export default Input;
