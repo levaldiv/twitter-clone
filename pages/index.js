0674f1a06591ebbaf98ddbf9a4af6eb0201f085e
Luis Valdivia <luis.edvaldivia@gmail.com>
2022-01-06 14:03:07 -0800

    added getserversideprops

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Feed from "../components/Feed";
 import Sidebar from "../components/Sidebar";
+import { getProviders, getSession, useSession } from "next-auth/react";
 
 export default function Home() {
   return (
@@ -13,7 +14,7 @@ export default function Home() {
 
       {/* Creating main component that will nest sub components */}
       {/* To have custom values just use []
-        * Ex: h-[400px] or bg-[#000] */}
+       * Ex: h-[400px] or bg-[#000] */}
       <main className="bg-black min-h-screen flex max-w-[1500px] mx-auto">
         {/* Sidebar */}
         <Sidebar />
@@ -25,4 +26,25 @@ export default function Home() {
       </main>
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  // trend and follow were created for the widgets comp; both fetching to an api endpoint
+  const trendingResults = await fetch("https://jsonkeeper.com/b/NKEV").then(
+    (res) => res.json()
+  );
+  const followResults = await fetch("https://jsonkeeper.com/b/WWMJ").then(
+    (res) => res.json()
+  );
+  const providers = await getProviders();
+  const session = await getSession(context);
+
+  return {
+    props: {
+      trendingResults,
+      followResults,
+      providers,
+      session,
+    },
+  };
 }
