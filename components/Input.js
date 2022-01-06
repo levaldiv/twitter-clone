c9426787c195ddee328e86cd7ef5cd402adca62b
Luis Valdivia <luis.edvaldivia@gmail.com>
2022-01-05 17:43:04 -0800

    uncommented sendpost

--- components/Input.js
+++ components/Input.js
@@ -98,7 +98,7 @@ function Input() {
 
   return (
     <div
-      className={`border-b border-gray-700 p-3 flex space-x-3 overflow-y-scroll`}
+      className={`border-b border-gray-700 p-3 flex space-x-3 overflow-y-scroll ${loading && "opacity-60"}`}
     >
       <img
         src="https://media-exp1.licdn.com/dms/image/D5635AQEyNbzjaS9vpw/profile-framedphoto-shrink_100_100/0/1641261133658?e=1641427200&v=beta&t=Q2BizxXqz07bAzGiayk2wsdeRiOaXeHZRZ_EvCFHVQ8"
@@ -195,7 +195,7 @@ function Input() {
                * input.trim removes the leading & trailing spaces ensuring that the btn stays disabled
                * when only spaces are added */
               disabled={!input.trim() && !selectedFile}
-              // onClick={sendPost}
+              onClick={sendPost}
             >
               Tweet
             </button>
