46770c13714edeaad66afb011a68a5ac2aab40bd
Luis Valdivia <luis.edvaldivia@gmail.com>
2022-01-05 16:13:57 -0800

    created firebase file

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDMFb5QD3SAeREvnntTcQ6S31mU3PvRrO8",
+    authDomain: "twitter-clone-38c67.firebaseapp.com",
+    projectId: "twitter-clone-38c67",
+    storageBucket: "twitter-clone-38c67.appspot.com",
+    messagingSenderId: "319399374764",
+    appId: "1:319399374764:web:ad8465f370219708141409"
+  };
