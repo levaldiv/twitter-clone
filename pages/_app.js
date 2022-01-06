14acb46374301d16b9e953305da37e1a5c41f282
Luis Valdivia <luis.edvaldivia@gmail.com>
2022-01-06 15:07:36 -0800

    added comments

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import "../styles/globals.css";
 import { SessionProvider } from "next-auth/react";
 
+// this is providing the sesion context globally to the whole app 
+// so at any point of the app, its exposing the session 
 export default function App({
   Component,
   pageProps: { session, ...pageProps }, 
