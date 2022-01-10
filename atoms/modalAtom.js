4def08ef2b2981a12c5b4886664f172c27a0f6e5
Luis Valdivia <luis.edvaldivia@gmail.com>
2022-01-10 15:37:05 -0800

    edited

--- atoms/modalAtom.js
+++ atoms/modalAtom.js
@@ -1,6 +1,12 @@
 import { atom } from "recoil";
 
+// This is avail globally into any component
 export const modalState = atom({
   key: "modalState",
   default: false,
+});
+
+export const postIdState = atom({
+  key: "postIdState",
+  default: "",
 });
