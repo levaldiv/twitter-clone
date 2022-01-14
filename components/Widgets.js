4417974ec1ee08c15ca0e89395255f60e4dd9f08
Luis Valdivia <luis.edvaldivia@gmail.com>
2022-01-14 12:29:54 -0800

    added divs

--- components/Widgets.js
+++ components/Widgets.js
@@ -1,5 +1,17 @@
+import { SearchIcon } from "@heroicons/react/outline";
+// import Trending from "./Trending";
+import Image from "next/image";
+
 function Widgets() {
-  return <div>sd</div>;
+  return (
+    <div className="hidden lg:inline ml-8 xl:w-[450px] py-1 space-y-5">
+      <div className="sticky top-0 py-1.5 bg-black z-50 w-11/12 xl:w-9/12">
+        <div className="flex items-center bg-[#202327] p-3 rounded-full relative">
+          <SearchIcon className="text-gray-500 h-5 z-50" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Widgets;
