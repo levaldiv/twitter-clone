5e16f8f7bb3fe59d438ed009b442a0bd7a8230e3
Luis Valdivia <luis.edvaldivia@gmail.com>
2022-01-13 16:50:51 -0800

    added post to show entire posr

--- pages/[id].js
+++ pages/[id].js
@@ -13,7 +13,7 @@ import { modalState } from "../atoms/modalAtom";
 import Modal from "../components/Modal";
 import Sidebar from "../components/Sidebar";
 // import Widgets from "../components/Widgets";
-// import Post from "../components/Post";
+import Post from "../components/Post";
 import { db } from "../firebase";
 import { ArrowLeftIcon } from "@heroicons/react/solid";
 // import Comment from "../components/Comment";
@@ -49,6 +49,22 @@ function PostPage({ trendingResults, followResults, providers }) {
 
       <main className="bg-black min-h-screen flex max-w-[1500px] mx-auto">
         <Sidebar />
+
+        <div className="flex-grow border-l border-r border-gray-700 max-w-2xl sm:ml-[73px] xl:ml-[370px]">
+          <div className="flex items-center px-1.5 py-2 border-b border-gray-700 text-[#d9d9d9] font-semibold text-xl gap-x-4 sticky top-0 z-50 bg-black">
+            <div
+              className="hoverAnimation w-9 h-9 flex items-center justify-center xl:px-0"
+              //   this routes me back to the home page
+              onClick={() => router.push("/")}
+            >
+              <ArrowLeftIcon className="h-5 text-white" />
+            </div>
+            Tweet
+          </div>
+
+          {/* this shows the entire post */}
+          <Post id={id} post={post} postPage />
+        </div>
         {isOpen && <Modal />}
       </main>
     </div>
