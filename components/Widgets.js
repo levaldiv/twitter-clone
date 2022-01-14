e29311128217404c8e07a6880faaa5793d209424
Luis Valdivia <luis.edvaldivia@gmail.com>
2022-01-14 12:49:09 -0800

    creted btn

--- components/Widgets.js
+++ components/Widgets.js
@@ -2,7 +2,7 @@ import { SearchIcon } from "@heroicons/react/outline";
 import Trending from "./Trending";
 import Image from "next/image";
 
-function Widgets() {
+function Widgets({ trendingResults, followResults }) {
   return (
     <div className="hidden lg:inline ml-8 xl:w-[450px] py-1 space-y-5">
       <div className="sticky top-0 py-1.5 bg-black z-50 w-11/12 xl:w-9/12">
@@ -21,6 +21,10 @@ function Widgets() {
         {trendingResults.map((result, index) => (
           <Trending key={index} result={result} />
         ))}
+
+        <button className="hover:bg-white hover:bg-opacity-[0.03] px-4 py-3 cursor-pointer transition duration-200 ease-out flex items-center justify-between w-full text-[#1d9bf0] font-light">
+          Show more
+        </button>
       </div>
     </div>
   );
