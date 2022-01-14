1bbe76ac6a33a91c5c488650cf9fa2de0f69427c
Luis Valdivia <luis.edvaldivia@gmail.com>
2022-01-14 13:21:38 -0800

    rendered img if there is one

--- components/Trending.js
+++ components/Trending.js
@@ -9,7 +9,29 @@ function Trending({ result }) {
         <h6 className="font-bold max-w-[250px] text-sm">
           {result.description}
         </h6>
+        <p className="text-[#6e767d] text-xs font-medium max-w-[250px]">
+          Trending with{" "}
+          {result.tags.map((tag, index) => (
+            <span className="tag" key={index}>
+              {tag}
+            </span>
+          ))}
+        </p>
       </div>
+
+      {result.img ? (
+        <Image
+          src={result.img}
+          width={70}
+          height={70}
+          objectFit="cover"
+          className="rounded-2xl"
+        />
+      ) : (
+        <div className="icon group">
+          <DotsHorizontalIcon className="h-5 text-[#6e767d] group-hover:text-[#1d9bf0]" />
+        </div>
+      )}
     </div>
   );
 }
