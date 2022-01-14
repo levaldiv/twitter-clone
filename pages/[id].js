76e03eb045f0e916fa6e918c32c81e96c18eced4
Luis Valdivia <luis.edvaldivia@gmail.com>
2022-01-13 16:28:58 -0800

    retrieving posts

--- pages/[id].js
+++ pages/[id].js
@@ -1,18 +1,43 @@
-import { useSession } from "next-auth/react";
-import Head from "next/head";
+import {
+  collection,
+  doc,
+  onSnapshot,
+  orderBy,
+  query,
+} from "@firebase/firestore";
+import { getProviders, getSession, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import { useRecoilState } from "recoil";
 import { modalState } from "../atoms/modalAtom";
 import Modal from "../components/Modal";
 import Sidebar from "../components/Sidebar";
+// import Widgets from "../components/Widgets";
+// import Post from "../components/Post";
+import { db } from "../firebase";
+import { ArrowLeftIcon } from "@heroicons/react/solid";
+// import Comment from "../components/Comment";
+import Head from "next/head";
 
 function PostPage() {
-  const { data: session } = useSessionSession();
+  const { data: session } = useSession();
   const [isOpen, setIsOpen] = useRecoilState(modalState);
+  const router = useRouter();
+  const { id } = router.query;
+
+  // getting the post
+  useEffect(
+    () =>
+      onSnapshot(doc(db, "posts", id), (snapshot) => {
+        setPost(snapshot.data());
+      }),
+    [db]
+  );
 
   return (
     <div className="">
       <Head>
-        <title>Twitter</title>
+        <title></title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
