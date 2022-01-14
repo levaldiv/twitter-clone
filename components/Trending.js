aaa73942f7dd798e41ead0fb4b3e78cdd2f499a9
Luis Valdivia <luis.edvaldivia@gmail.com>
2022-01-14 13:23:29 -0800

    added comments

--- components/Trending.js
+++ components/Trending.js
@@ -5,6 +5,7 @@ function Trending({ result }) {
   return (
     <div className="hover:bg-white hover:bg-opacity-[0.03] px-4 py-2 cursor-pointer transition duration-200 ease-out flex items-center justify-between">
       <div className="space-y-0.5">
+        {/* we are getting all this info (result.heading, result.description etc..) from JSONKEEPER */}
         <p className="text-[#6e767d] text-xs font-medium">{result.heading}</p>
         <h6 className="font-bold max-w-[250px] text-sm">
           {result.description}
