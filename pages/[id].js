b17153fae91a4a1d0fd2569c910d66aff96ee0c1
Luis Valdivia <luis.edvaldivia@gmail.com>
2022-01-13 16:20:30 -0800

    added basic skeleton

--- pages/[id].js
+++ pages/[id].js
@@ -1,5 +1,27 @@
+import { useSession } from "next-auth/react";
+import Head from "next/head";
+import { useRecoilState } from "recoil";
+import { modalState } from "../atoms/modalAtom";
+import Modal from "../components/Modal";
+import Sidebar from "../components/Sidebar";
+
 function PostPage() {
-  return <div></div>;
+  const { data: session } = useSessionSession();
+  const [isOpen, setIsOpen] = useRecoilState(modalState);
+
+  return (
+    <div className="">
+      <Head>
+        <title>Twitter</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="bg-black min-h-screen flex max-w-[1500px] mx-auto">
+        <Sidebar />
+        {isOpen && <Modal />}
+      </main>
+    </div>
+  );
 }
 
 export default PostPage;
