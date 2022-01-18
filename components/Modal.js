0930d6a21338ba170f9f2c107f2c6e703a7605ed
Luis Valdivia <luis.edvaldivia@gmail.com>
2022-01-18 15:05:36 -0800

    moved some styles to global.css

--- components/Modal.js
+++ components/Modal.js
@@ -65,6 +65,7 @@ function Modal() {
     <Transition.Root show={isOpen} as={Fragment}>
       <Dialog as="div" className="fixed z-50 inset-0 pt-8" onClose={setIsOpen}>
         <div className="flex items-start justify-center min-h-[800px] sm:min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+          
           <Transition.Child
             as={Fragment}
             enter="ease-out duration-300"
@@ -72,8 +73,7 @@ function Modal() {
             enterTo="opacity-100"
             leave="ease-in duration-200"
             leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
+            leaveTo="opacity-0" >
             <Dialog.Overlay className="fixed inset-0 bg-[#5b7083] bg-opacity-40 transition-opacity" />
           </Transition.Child>
 
@@ -84,15 +84,15 @@ function Modal() {
             enterTo="opacity-100 translate-y-0 sm:scale-100"
             leave="ease-in duration-200"
             leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-            leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-          >
-            <div className="inline-block align-bottom bg-black rounded-2xl text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-xl sm:w-full">
+            leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" >
+            
+            <div className="popupBkgd">
               <div className="flex items-center px-1.5 py-2 border-b border-gray-700">
                 <div
                   className="hoverAnimation w-9 h-9 flex items-center justify-center xl:px-0"
                   // this lets user click the x icon and close the modal (changing globally)
-                  onClick={() => setIsOpen(false)}
-                >
+                  onClick={() => setIsOpen(false)} >
+                  
                   <XIcon className="h-[22px] text-white" />
                 </div>
               </div>
@@ -100,27 +100,31 @@ function Modal() {
               <div className="flex px-4 pt-5 pb-2.5 sm:px-6">
                 <div className="w-full">
                   <div className="text-[#6e767d] flex gap-x-3 relative">
+                    
                     <span className="w-0.5 h-full z-[-1] absolute left-5 top-11 bg-gray-600" />
                     <img
                       src={post?.userImg}
                       alt=""
-                      className="h-11 w-11 rounded-full"
-                    />
+                      className="h-11 w-11 rounded-full" />
 
                     <div>
                       <div className="inline-block group">
+                        
                         <h4 className="font-bold text-[#d9d9d9] inline-block text-[15px] sm:text-base">
                           {post?.username}
                         </h4>
+                        
                         <span className="ml-1.5 text-sm sm:text-[15px]">
                           {" "}
                           @{post?.tag}{" "}
                         </span>
                       </div>{" "}
                       ·{" "}
+                      
                       <span>
                         <Moment fromNow>{post?.timestamp?.toDate()}</Moment>
                       </span>
+                      
                       <p className="text-[#d9d9d9] text-[15px] sm:text-base">
                         {post?.text}
                       </p>
@@ -131,8 +135,7 @@ function Modal() {
                     <img
                       // this will be the image for the person who is going to reply to the post
                       src={session.user.image}
-                      className="h-11 w-11 rounded-full"
-                    />
+                      className="h-11 w-11 rounded-full" />
 
                     <div className="flex-grow mt-2">
                       {/* This is where the user will post their reply to a previously made post */}
@@ -142,8 +145,7 @@ function Modal() {
                         onChange={(e) => setComment(e.target.value)}
                         placeholder="Tweet your reply"
                         rows="2"
-                        className="bg-transparent outline-none text-[#d9d9d9] text-lg placeholder-gray-500 tracking-wide w-full min-h-[80px]"
-                      />
+                        className="tweetComm" />
 
                       <div className="flex items-center justify-between pt-2.5">
                         <div className="flex items-center">
@@ -165,12 +167,11 @@ function Modal() {
                         </div>
 
                         <button
-                          className="bg-[#1d9bf0] text-white rounded-full px-4 py-1.5 font-bold shadow-md hover:bg-[#1a8cd8] disabled:hover:bg-[#1d9bf0] disabled:opacity-50 disabled:cursor-default"
+                          className="replyBtn"
                           type="submit"
                           // When Reply is clicked, triger sendComment function
                           onClick={sendComment}
-                          disabled={!comment.trim()}
-                        >
+                          disabled={!comment.trim()} >
                           Reply
                         </button>
                       </div>
