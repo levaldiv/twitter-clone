9cccabcf19e77c11dd40ee1828e58cfb5267dc2e
Luis Valdivia <luis.edvaldivia@gmail.com>
2022-01-18 11:11:19 -0800

    moved some styles to global.css

--- components/Comment.js
+++ components/Comment.js
@@ -7,33 +7,35 @@ import {
 } from "@heroicons/react/outline";
 import Moment from "react-moment";
 
-function Comment({ id, comment }) {
+function Comment({ comment }) {
   return (
     <div className="p-3 flex cursor-pointer border-b border-gray-700">
-      <img
-        src={comment?.userImg}
-        alt=""
-        className="h-11 w-11 rounded-full mr-4"
-      />
+      <img src={comment?.userImg} alt="" className="userImage" />
+
       <div className="flex flex-col space-y-2 w-full">
         <div className="flex justify-between">
           <div className="text-[#6e767d]">
             <div className="inline-block group">
+              
               <h4 className="font-bold text-[#d9d9d9] text-[15px] sm:text-base inline-block group-hover:underline">
                 {comment?.username}
               </h4>
+              
               <span className="ml-1.5 text-sm sm:text-[15px]">
                 @{comment?.tag}{" "}
               </span>
             </div>{" "}
             ·{" "}
+            
             <span className="hover:underline text-sm sm:text-[15px]">
               <Moment fromNow>{comment?.timestamp?.toDate()}</Moment>
             </span>
+            
             <p className="text-[#d9d9d9] mt-0.5 max-w-lg text-[15px] sm:text-base">
               {comment?.comment}
             </p>
           </div>
+          
           <div className="icon group flex-shrink-0">
             <DotsHorizontalIcon className="h-5 text-[#6e767d] group-hover:text-[#1d9bf0]" />
           </div>
