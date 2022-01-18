e179e53a131d3a7d42106f55e7f935015474bbd8
Luis Valdivia <luis.edvaldivia@gmail.com>
2022-01-18 15:51:36 -0800

    moved some styles to global.css

--- components/Trending.js
+++ components/Trending.js
@@ -3,13 +3,17 @@ import Image from "next/image";
 
 function Trending({ result }) {
   return (
-    <div className="hover:bg-white hover:bg-opacity-[0.03] px-4 py-2 cursor-pointer transition duration-200 ease-out flex items-center justify-between">
+    <div className="bkgdAnimation">
+      
       <div className="space-y-0.5">
+        
         {/* we are getting all this info (result.heading, result.description etc..) from JSONKEEPER */}
         <p className="text-[#6e767d] text-xs font-medium">{result.heading}</p>
+        
         <h6 className="font-bold max-w-[250px] text-sm">
           {result.description}
         </h6>
+        
         <p className="text-[#6e767d] text-xs font-medium max-w-[250px]">
           Trending with{" "}
           {result.tags.map((tag, index) => (
@@ -26,9 +30,9 @@ function Trending({ result }) {
           width={70}
           height={70}
           objectFit="cover"
-          className="rounded-2xl"
-        />
+          className="rounded-2xl" />
       ) : (
+        
         <div className="icon group">
           <DotsHorizontalIcon className="h-5 text-[#6e767d] group-hover:text-[#1d9bf0]" />
         </div>
